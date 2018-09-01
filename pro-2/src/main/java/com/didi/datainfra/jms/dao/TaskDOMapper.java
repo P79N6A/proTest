e0709e1c91e154331fcef08a9697bbf2ffd935c6
wanghongbing <wanghongbing@didichuxing.com>
2018-09-01 14:58:27 +0800

    修改为Mybastis3Simple模式，只生成3个文件，简洁

--- pro-2/src/main/java/com/didi/datainfra/jms/dao/TaskDOMapper.java
+++ pro-2/src/main/java/com/didi/datainfra/jms/dao/TaskDOMapper.java
@@ -1,37 +1,16 @@
 package com.didi.datainfra.jms.dao;
 
 import com.didi.datainfra.jms.domain.entity.TaskDO;
-import com.didi.datainfra.jms.domain.entity.TaskDOExample;
-import com.didi.datainfra.jms.domain.entity.TaskDOWithBLOBs;
 import java.util.List;
-import org.apache.ibatis.annotations.Param;
 
 public interface TaskDOMapper {
-    long countByExample(TaskDOExample example);
-
-    int deleteByExample(TaskDOExample example);
-
     int deleteByPrimaryKey(Long id);
 
-    int insert(TaskDOWithBLOBs record);
+    int insert(TaskDO record);
 
-    int insertSelective(TaskDOWithBLOBs record);
+    TaskDO selectByPrimaryKey(Long id);
 
-    List<TaskDOWithBLOBs> selectByExampleWithBLOBs(TaskDOExample example);
-
-    List<TaskDO> selectByExample(TaskDOExample example);
-
-    TaskDOWithBLOBs selectByPrimaryKey(Long id);
-
-    int updateByExampleSelective(@Param("record") TaskDOWithBLOBs record, @Param("example") TaskDOExample example);
-
-    int updateByExampleWithBLOBs(@Param("record") TaskDOWithBLOBs record, @Param("example") TaskDOExample example);
-
-    int updateByExample(@Param("record") TaskDO record, @Param("example") TaskDOExample example);
-
-    int updateByPrimaryKeySelective(TaskDOWithBLOBs record);
-
-    int updateByPrimaryKeyWithBLOBs(TaskDOWithBLOBs record);
+    List<TaskDO> selectAll();
 
     int updateByPrimaryKey(TaskDO record);
 }
--- pro-2/src/main/java/com/didi/datainfra/jms/domain/entity/TaskDO.java
+++ pro-2/src/main/java/com/didi/datainfra/jms/domain/entity/TaskDO.java
@@ -91,6 +91,12 @@ public class TaskDO {
 
     private Boolean cascade;
 
+    private String inputPaths;
+
+    private String outputPaths;
+
+    private String remark;
+
     public Long getId() {
         return id;
     }
@@ -441,5 +447,29 @@ public class TaskDO {
 
     public void setCascade(Boolean cascade) {
         this.cascade = cascade;
+    }
+
+    public String getInputPaths() {
+        return inputPaths;
+    }
+
+    public void setInputPaths(String inputPaths) {
+        this.inputPaths = inputPaths;
+    }
+
+    public String getOutputPaths() {
+        return outputPaths;
+    }
+
+    public void setOutputPaths(String outputPaths) {
+        this.outputPaths = outputPaths;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
     }
 }
